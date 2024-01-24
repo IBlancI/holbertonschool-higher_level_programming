f0bf429e7a4994613773a1160b9ee66006620655
IBlancI <adilslimani93130@gmail.com>
2024-01-24 12:20:43 +0100

    Task fixed

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
-
-for i in range(ord('a'), ord('z') + 1):
-    if chr(i) not in ['q', 'e']:
-        print(chr(i), end='')
-
+for i in range(97, 123):
+    print("{:c}".format(i), end='')
