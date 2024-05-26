0c5e69e069e68767aefb02b89538ead1c43700f9
IBlancI <adilslimani93130@gmail.com>
2024-05-26 18:17:49 +0200

    task fixed

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Write a function that prints My name is <first name> <last name>"""
 
+
 def say_my_name(first_name, last_name=""):
     """
     function that prints name
@@ -9,9 +10,8 @@ def say_my_name(first_name, last_name=""):
         TypeError:first_name must be a string
         TypeError:last_name must be a string
     """
-    if not isinstance(first_name, str):
+    if not isinstance(first_name, (str)):
         raise TypeError("first_name must be a string")
-    if not isinstance(last_name, str):
+    if not isinstance(last_name, (str)):
         raise TypeError("last_name must be a string")
-    print("My name {:s} {:s}".format(first_name, last_name))
-
+    print("My name is {:s} {:s}".format(first_name, last_name))
