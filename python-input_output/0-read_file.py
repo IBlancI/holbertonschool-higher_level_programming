43451d235e1b97ceb541d08ad2ae112861388dcd
IBlancI <adilslimani93130@gmail.com>
2024-05-28 23:28:17 +0200

    task fixed

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-"""function to read a file"""
+"""function that reads a file"""
 
 
 def read_file(filename=""):
     """Read_file"""
-    with open('my_file_0.txt', encoding="utf-8") as file:
+    with open(filename, 'r', encoding='utf-8') as file:
         content = file.read()
         print(content, end="")
