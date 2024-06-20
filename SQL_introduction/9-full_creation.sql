191aac032de8086dbf18a36df8ee23f18c676abf
IBlancI <adilslimani93130@gmail.com>
2024-06-20 21:48:35 +0200

    task added

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,11 +1,13 @@
--- Script that creates a table second_table
--- Adds multiple rows in second_table
+-- Create table second_table if it does not exist
 CREATE TABLE IF NOT EXISTS second_table (
     id INT PRIMARY KEY,
     name VARCHAR(256),
     score INT
 );
-INSERT INTO second_table (id, name, score) VALUES (1, 'John', 10);
-INSERT INTO second_table (id, name, score) VALUES (2, 'Alex', 3);
-INSERT INTO second_table (id, name, score) VALUES (3, 'Bob', 14);
-INSERT INTO second_table (id, name, score) VALUES (4, 'George', 8);
+
+-- Insert rows into second_table
+INSERT IGNORE INTO second_table (id, name, score) VALUES 
+(1, 'John', 10),
+(2, 'Alex', 3),
+(3, 'Bob', 14),
+(4, 'George', 8);
