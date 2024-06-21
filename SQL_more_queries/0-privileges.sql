4707d1da2b3f1ce3e8e3e9be51ed490d9e4544ca
IBlancI <adilslimani93130@gmail.com>
2024-06-21 21:13:24 +0200

    test of task

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,3 +1,4 @@
--- TASK : Lists all privileges of the MySQL users user_0d_1 and user_0d_2 on your server (in localhost).
-SHOW GRANTS FOR "user_0d_1"@"localhost";
-SHOW GRANTS FOR "user_0d_2"@"localhost";
+-- create some script
+-- in MYSQL !!!
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
